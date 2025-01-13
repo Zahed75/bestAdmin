d000825043e69e627750a7ceb69b09cc4aef9856
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-14 01:06:06 +0600

    project setup done

---
+++ src/app/components/outlets/outlets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outlets',
+  imports: [],
+  templateUrl: './outlets.component.html',
+  styleUrl: './outlets.component.css'
+})
+export class OutletsComponent {
+
+}
