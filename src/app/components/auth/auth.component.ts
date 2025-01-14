b5f3265a026c5a79ac71a74fac9ccc8ebd923081
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-15 02:13:43 +0600

    sign in api fetched

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
-  showPassword: boolean = false;
+  loginData = {
+    email: '',
+    password: '',
+  };
 
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  onLogin() {
+    this.authService.signInAdmin(this.loginData).subscribe(
+      (res: any) => {
+        if (res.user?.accessToken) {
+          localStorage.setItem('accessToken', res.user.accessToken);
+          localStorage.setItem('refreshToken', res.user.refreshToken);
+          localStorage.setItem('userId', res.user.userId);
+          localStorage.setItem('email', res.user.email);
+          localStorage.setItem('role', res.user.role);
+          localStorage.setItem('users', JSON.stringify(res.user));
+          this.router.navigateByUrl('/layout/dashboard');
+        } else {
+          alert(res.message || 'Login failed.');
+        }
+      },
+      (err) => {
+        alert(err.error?.message || 'An error occurred. Please try again.');
+      }
+    );
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private baseURL = 'https://app.bestelectronics.com.bd/api/v1/auth';
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  signInAdmin(loginData: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.baseURL}/signInAdmin`, loginData);
+  }
+
 }
