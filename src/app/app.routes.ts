7771e351c675345bcea2845c46f8fde60421d588
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-14 22:55:25 +0600

    route setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,68 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AuthComponent} from './components/auth/auth.component';
+import {LayoutComponent} from './components/layout/layout.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {CustomersComponent} from './components/customers/customers.component';
+import {OrdersComponent} from './components/orders/orders.component';
+import {OutletsComponent} from './components/outlets/outlets.component';
+import {DiscountComponent} from './components/discount/discount.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {UsersComponent} from './components/users/users.component';
+import {ReportingComponent} from './components/reporting/reporting.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'customers',
+        component: CustomersComponent,
+      },
+      {
+        path: 'order',
+        component: OrdersComponent
+      },
+      {
+        path: 'outlets',
+        component: OutletsComponent
+      },
+      {
+        path: 'discount',
+        component: DiscountComponent
+      },
+      {
+        path: 'product-details',
+        component: ProductDetailsComponent
+      },
+      {
+        path: 'users',
+        component: UsersComponent
+      },
+      {
+        path: 'reporting',
+        component: ReportingComponent
+      },
+      {
+        path:'**',
+        redirectTo:'login'
+      }
+
+    ]
+  }
+
+];
