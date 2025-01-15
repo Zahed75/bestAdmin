7e60d73330d778990f968a05db76f0fe4cfe21e0
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-16 02:59:24 +0600

    pagination added

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,46 +1,5 @@
-// import { Component, OnInit } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { OrdersService } from '../../services/orders.service';
-// import {CurrencyPipe, DatePipe, DecimalPipe, NgFor, NgIf} from '@angular/common';
-//
-// @Component({
-//   selector: 'app-orders',
-//   imports: [
-//     RouterOutlet,
-//     NgFor,
-//     DatePipe,
-//     DecimalPipe,
-//
-//
-//   ],
-//   templateUrl: './orders.component.html',
-//   styleUrls: ['./orders.component.css'] // Fixed typo: `styleUrl` to `styleUrls`
-// })
-// export class OrdersComponent implements OnInit {
-//   orders: any[] = [];
-//   isLoading: boolean = true;
-//
-//   constructor(private ordersService: OrdersService) {}
-//
-//   ngOnInit(): void {
-//     this.ordersService.getOrders().subscribe({
-//       next: (data) => {
-//         console.log('Fetched Orders:', data); // Debug log
-//         this.orders = data; // Assign the fetched orders
-//         this.isLoading = false; // Stop loading spinner
-//       },
-//       error: (error) => {
-//         console.error('Error fetching orders:', error);
-//         this.isLoading = false; // Stop loading spinner in case of error
-//       }
-//     });
-//   }
-// }
-
-
-
-import { Component, OnInit } from '@angular/core';
-import { OrdersService } from '../../services/orders.service';
+import {Component, OnInit} from '@angular/core';
+import {OrdersService} from '../../services/orders.service';
 import {RouterOutlet} from '@angular/router';
 import {DatePipe, DecimalPipe, NgClass, NgFor} from '@angular/common';
 
@@ -63,7 +22,8 @@ export class OrdersComponent implements OnInit {
   itemsPerPage: number = 10;
   totalPages: number = 0;
 
-  constructor(private ordersService: OrdersService) {}
+  constructor(private ordersService: OrdersService) {
+  }
 
   ngOnInit(): void {
     this.ordersService.getOrders().subscribe({
@@ -74,6 +34,8 @@ export class OrdersComponent implements OnInit {
       error: (error) => console.error('Error fetching orders:', error),
     });
   }
+
+  // Pagination Start
 
   updatePagination(): void {
     this.totalPages = Math.ceil(this.orders.length / this.itemsPerPage);
@@ -107,8 +69,10 @@ export class OrdersComponent implements OnInit {
     const startPage = Math.max(1, this.currentPage - 2);
     const endPage = Math.min(this.totalPages, startPage + 4);
     return Array.from(
-      { length: endPage - startPage + 1 },
+      {length: endPage - startPage + 1},
       (_, i) => startPage + i
     );
   }
+
+//   pagination End
 }
