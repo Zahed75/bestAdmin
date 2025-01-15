d67ada29d981878da3bcf1342feee38fcdc12bb2
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-15 13:20:03 +0600

    gloabl URL set

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,19 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../enviornments/environment';
+
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private baseURL = 'https://app.bestelectronics.com.bd/api/v1/auth';
+  private baseURL = `${environment.apiBaseUrl}/auth`;
 
   private http = inject(HttpClient);
 
   signInAdmin(loginData: { email: string; password: string }): Observable<any> {
     return this.http.post(`${this.baseURL}/signInAdmin`, loginData);
   }
-
 }
--- src/enviornments/environment.ts
+++ src/enviornments/environment.ts
@@ -2,3 +2,12 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://app.bestelectronics.com.bd/api/v1'
 };
+
+
+
+
+// Development environment configuration
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://localhost:3000/api/v1' // Local development URL
+// };
