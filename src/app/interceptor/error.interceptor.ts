ecf5c3b8e24b208bde8d0b03ff35b93fad45d6c7
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-15 13:24:56 +0600

    token interceptor/error interceptor added

--- src/app/interceptor/error.interceptor.ts
+++ src/app/interceptor/error.interceptor.ts
@@ -1,5 +1,24 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { DashboardService } from '../components/dashboard/dashboard.service'; // Correct path
+
+
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const dashService = inject(DashboardService); // Inject service
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        const isContinue = confirm(
+          'Your session has expired. Do you want to login again?'
+        );
+        if (isContinue) {
+          dashService.tokenExpired$.next(true);
+        }
+      }
+      return throwError(() => error);
+    })
+  );
 };
