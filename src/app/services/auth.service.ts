79087b26d55de7c2f9332c9d8753f3cf4028c531
Zahed75 <zahedhasan.cs@gmail.com>
2025-01-23 16:07:54 +0600

    rolePermission added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,3 @@
-// import { Injectable, inject } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { environment } from '../../enviornments/environment';
-//
-//
-//
-// @Injectable({
-//   providedIn: 'root',
-// })
-//
-//
-// export class AuthService {
-//   private baseURL = `${environment.apiBaseUrl}/auth`;
-//
-//   private http = inject(HttpClient);
-//
-//   signInAdmin(loginData: { email: string; password: string }): Observable<any> {
-//     return this.http.post(`${this.baseURL}/signInAdmin`, loginData);
-//   }
-// }
-
-
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
