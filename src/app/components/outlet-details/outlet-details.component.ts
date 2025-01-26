8e1c52e73fd4ee8c91bf81219570467f5fa973be
Zahed75 <zahedhasan.cs@gmail.com>
2025-01-27 00:38:32 +0600

    outlet details fixed

--- src/app/components/outlet-details/outlet-details.component.ts
+++ src/app/components/outlet-details/outlet-details.component.ts
@@ -1,77 +1,3 @@
-// import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-// import {ActivatedRoute, RouterLink} from '@angular/router';
-// import {FormsModule} from '@angular/forms';
-// import {OutletService} from '../../services/outlet.service';
-// import {NgFor} from '@angular/common';
-//
-// @Component({
-//   selector: 'app-outlet-details',
-//   imports: [
-//     RouterLink,
-//     FormsModule,
-//     NgFor
-//   ],
-//   templateUrl: './outlet-details.component.html',
-//   styleUrl: './outlet-details.component.css'
-// })
-// export class OutletDetailsComponent implements OnInit {
-//   outlet: any = {};
-//   isLoading: boolean = false;
-//   errorMessage: string = '';
-//   managers: any[] = [];
-//   constructor(
-//     private outletService: OutletService,
-//     private cdr: ChangeDetectorRef,
-//     private route: ActivatedRoute,
-//   ) {
-//   }
-//
-//   ngOnInit() {
-//     const outletId = this.route.snapshot.paramMap.get('outletId');
-//     if (outletId) {
-//       this.fetchOutletDetails(outletId);
-//       this.getAllManagers();
-//     }
-//   }
-//
-//
-//   fetchOutletDetails(outletId: string): void {
-//     this.isLoading = true;
-//     this.outletService.getOutletById(outletId).subscribe({
-//       next: (response) => {
-//         this.outlet = response.outlet;
-//         this.outlet.id= outletId;
-//         this.isLoading = false;
-//       },
-//       error: (error) => {
-//         console.error('Error fetching outlet details:', error);
-//         this.errorMessage = 'Failed to load outlet details. Please try again later.';
-//         this.isLoading = false;
-//       },
-//     });
-//   }
-//
-//   getAllManagers(): void {
-//     this.isLoading = true;
-//     this.outletService.getAllManagers().subscribe({
-//       next: (managers: any[]) => {
-//         this.managers = managers;
-//         this.isLoading = false;
-//       },
-//       error: (error) => {
-//         console.error('Error fetching managers:', error);
-//         this.errorMessage = 'Failed to load managers. Please try again later.';
-//         this.isLoading = false;
-//       },
-//     });
-//   }
-//
-// }
-//
-//
-
-
-
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -93,7 +19,6 @@ export class OutletDetailsComponent implements OnInit {
 
   constructor(
     private outletService: OutletService,
-    private cdr: ChangeDetectorRef,
     private route: ActivatedRoute
   ) {}
 
