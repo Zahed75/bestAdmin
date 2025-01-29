70461c8a59de51979342af70dc4fc0a010111444
Zahed75 <zahedhasan.cs@gmail.com>
2025-01-29 16:16:17 +0600

    ckeditor configured

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,29 +1,256 @@
-import {Component, OnInit} from '@angular/core';
-import {NgFor, NgIf} from '@angular/common';
+import { CommonModule, NgFor, NgIf} from '@angular/common';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  loadCKEditorCloud,
+  CKEditorModule,
+  type CKEditorCloudResult,
+  type CKEditorCloudConfig,
+} from '@ckeditor/ckeditor5-angular';
+
+import type {
+  ClassicEditor,
+  EditorConfig,
+} from 'https://cdn.ckeditor.com/typings/ckeditor5.d.ts';
+
+const LICENSE_KEY =
+  'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3Njk2NDQ3OTksImp0aSI6IjliY2M4ODM1LTc3ZjMtNDMwOC1hZjIxLTgzYzQxNGNmOTc5OCIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiMmM0MmQ2ZDIifQ.TkcaS4YFgY77J7GHhjKgr5PsHSsWbPMPRAdrIj8zrhoa1bdoUEW9aSdfIS6AM4Dq0x9DUz_aBw5Uyk8txwfrlw';
+
+const CLOUD_SERVICES_TOKEN_URL =
+  'https://t9cw67u3pmtj.cke-cs.com/token/dev/52636a8ca1c4ef819003d04298b00555dd27545c215f07c4b219ba398ed8?limit=10';
+
+const cloudConfig = {
+  version: '44.1.0',
+  ckbox: {
+    version: '2.6.1',
+  },
+  premium: true,
+} satisfies CKEditorCloudConfig;
+
 
 @Component({
   selector: 'app-add-product',
+  standalone: true,
   imports: [
-    NgIf,
-    NgFor
-  ],
+     CKEditorModule,
+     NgIf,
+     NgFor,
+     FormsModule,
+     CommonModule
+    ],
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.css'
+  styleUrl: './add-product.component.css',
+  encapsulation: ViewEncapsulation.None
 })
-export class AddProductComponent implements OnInit {
 
+
+
+export class AddProductComponent implements OnInit {
   featuredImage: string | ArrayBuffer | null = '';
   galleryImages: string[] = [];
   isGalleryModalOpen: boolean = false;
 
+  public Editor: typeof ClassicEditor | null = null;
+	public config: EditorConfig | null = null;
+
+
+	
+
+	private _setupEditor(cloud: CKEditorCloudResult<typeof cloudConfig>) {
+		const {
+			ClassicEditor,
+			AutoImage,
+			Autosave,
+			BlockQuote,
+			Bold,
+			CKBox,
+			CKBoxImageEdit,
+			CloudServices,
+			Essentials,
+			Heading,
+			ImageBlock,
+			ImageCaption,
+			ImageInline,
+			ImageInsert,
+			ImageInsertViaUrl,
+			ImageResize,
+			ImageStyle,
+			ImageTextAlternative,
+			ImageToolbar,
+			ImageUpload,
+			Indent,
+			IndentBlock,
+			Italic,
+			Link,
+			LinkImage,
+			List,
+			ListProperties,
+			Paragraph,
+			PictureEditing,
+			SpecialCharacters,
+			TodoList,
+			Underline
+		} = cloud.CKEditor;
+
+
+		this.Editor = ClassicEditor;
+		this.config = {
+			toolbar: {
+				items: [
+					'heading',
+					'|',
+					'bold',
+					'italic',
+					'underline',
+					'|',
+					'specialCharacters',
+					'link',
+					'insertImage',
+					'ckbox',
+					'blockQuote',
+					'|',
+					'bulletedList',
+					'numberedList',
+					'todoList',
+					'outdent',
+					'indent'
+				],
+				shouldNotGroupWhenFull: false
+			},
+			plugins: [
+				AutoImage,
+				Autosave,
+				BlockQuote,
+				Bold,
+				CKBox,
+				CKBoxImageEdit,
+				CloudServices,
+				Essentials,
+				Heading,
+				ImageBlock,
+				ImageCaption,
+				ImageInline,
+				ImageInsert,
+				ImageInsertViaUrl,
+				ImageResize,
+				ImageStyle,
+				ImageTextAlternative,
+				ImageToolbar,
+				ImageUpload,
+				Indent,
+				IndentBlock,
+				Italic,
+				Link,
+				LinkImage,
+				List,
+				ListProperties,
+				Paragraph,
+				PictureEditing,
+				SpecialCharacters,
+				TodoList,
+				Underline
+			],
+			cloudServices: {
+				tokenUrl: CLOUD_SERVICES_TOKEN_URL
+			},
+			heading: {
+				options: [
+					{
+						model: 'paragraph',
+						title: 'Paragraph',
+						class: 'ck-heading_paragraph'
+					},
+					{
+						model: 'heading1',
+						view: 'h1',
+						title: 'Heading 1',
+						class: 'ck-heading_heading1'
+					},
+					{
+						model: 'heading2',
+						view: 'h2',
+						title: 'Heading 2',
+						class: 'ck-heading_heading2'
+					},
+					{
+						model: 'heading3',
+						view: 'h3',
+						title: 'Heading 3',
+						class: 'ck-heading_heading3'
+					},
+					{
+						model: 'heading4',
+						view: 'h4',
+						title: 'Heading 4',
+						class: 'ck-heading_heading4'
+					},
+					{
+						model: 'heading5',
+						view: 'h5',
+						title: 'Heading 5',
+						class: 'ck-heading_heading5'
+					},
+					{
+						model: 'heading6',
+						view: 'h6',
+						title: 'Heading 6',
+						class: 'ck-heading_heading6'
+					}
+				]
+			},
+			image: {
+				toolbar: [
+					'toggleImageCaption',
+					'imageTextAlternative',
+					'|',
+					'imageStyle:inline',
+					'imageStyle:wrapText',
+					'imageStyle:breakText',
+					'|',
+					'resizeImage',
+					'|',
+					'ckboxImageEdit'
+				]
+			},
+			initialData:
+				'<h2>Write the Product Desc..</h2>',
+			licenseKey: LICENSE_KEY,
+			link: {
+				addTargetToExternalLinks: true,
+				defaultProtocol: 'https://',
+				decorators: {
+					toggleDownloadable: {
+						mode: 'manual',
+						label: 'Downloadable',
+						attributes: {
+							download: 'file'
+						}
+					}
+				}
+			},
+			list: {
+				properties: {
+					styles: true,
+					startIndex: true,
+					reversed: true
+				}
+			},
+
+			placeholder: 'Type or paste your content here!'
+		};
+
+		
+	}
+
 
   ngOnInit() {
+    loadCKEditorCloud(cloudConfig)
+      .then(cloud => this._setupEditor(cloud))
+      .catch(error => console.error('Error loading CKEditor:', error));
   }
+  
 
-
-  constructor() {
-  }
-
+  constructor() {}
 
   // Handle Featured Image Upload
   onFeaturedImageChange(event: Event): void {
@@ -63,7 +290,11 @@ export class AddProductComponent implements OnInit {
 
   // Remove an Image
   removeImage(imgSrc: string): void {
-    this.galleryImages = this.galleryImages.filter(img => img !== imgSrc);
+    this.galleryImages = this.galleryImages.filter((img) => img !== imgSrc);
   }
 
+
 }
+
+
+
