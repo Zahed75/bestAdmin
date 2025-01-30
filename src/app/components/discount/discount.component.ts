2fb8ef25e10b37ce7eab58996825727a08278263
Zahed75 <zahedhasan.cs@gmail.com>
2025-01-30 09:46:49 +0600

    reporting ui done

--- src/app/components/discount/discount.component.ts
+++ src/app/components/discount/discount.component.ts
@@ -6,7 +6,6 @@ import {RouterLink} from '@angular/router';
   selector: 'app-discount',
   imports: [
     FormsModule,
-    RouterLink
   ],
   templateUrl: './discount.component.html',
   styleUrl: './discount.component.css'
