482a18a53b35df9d0fae60e3b30e7e9aee2288a1
Zahed75 <zahedhasan.cs@gmail.com>
2025-01-30 09:40:36 +0600

    reporting ui done

--- src/app/components/reporting/reporting.component.ts
+++ src/app/components/reporting/reporting.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-reporting',
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './reporting.component.html',
   styleUrl: './reporting.component.css'
 })
