ba517a9c558ceb68ec839e102059a36a3c045c72
Zahed75 <zahedhasan.cs@gmail.com>
2025-02-02 14:49:17 +0600

    GetAllCategories service added

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@angular/core';
-
+import {inject, Injectable} from '@angular/core';
+import {environment} from '../../../enviornments/environment';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GetAllCategoriesResponse } from '../../model/category.model';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  private getAllCategoriesURL=`${environment.apiBaseUrl}/category/getAllCat`;
+  private http = inject(HttpClient);
 
+
+
+  getAllCategories():Observable<GetAllCategoriesResponse>{
+    return this.http.get<GetAllCategoriesResponse>(this.getAllCategoriesURL)
+  }
 
 }
